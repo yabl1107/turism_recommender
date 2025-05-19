bbbca077b7eb52109a86106f630867ced646d930
Abel <abel07110711@gmail.com>
2025-05-19 11:00:22 -0500

    redis memory added

--- main.py
+++ main.py
@@ -6,13 +6,27 @@ from dotenv import load_dotenv
 import openai
 from openai import OpenAI
 import os
+import redis
+import json
+
 
 load_dotenv()
+# Obtiene los valores
+redis_host = os.getenv("REDIS_HOST")
+redis_port = int(os.getenv("REDIS_PORT"))
+redis_password = os.getenv("REDIS_PASSWORD")
 
 # Carga la base de datos persistida
 db_turismo = Chroma(persist_directory="./bd2", embedding_function=OpenAIEmbeddings())
 data = pd.read_csv("clean_data.csv")
 
+# Conexión a Redis Cloud
+redis_client = redis.Redis(
+    host= redis_host,
+    port=redis_port,
+    password=redis_password,
+    decode_responses=True
+)
 
 app = Flask(__name__)
 @app.route('/recomendar', methods=['POST'])
@@ -21,12 +35,40 @@ def recomendar():
     query = body.get("query")
     top_k = body.get("top_k", 2)
 
+    user_id = body.get("user_id", "anonimo")
+    chat_id = body.get("chat_id", "default")
+
     if not query:
         return jsonify({"error": "Falta el campo 'query'"}), 400
+    if not user_id:
+        return jsonify({"error": "Falta el campo 'user_id'"}), 400
+    if not chat_id:
+        return jsonify({"error": "Falta el campo 'chat_id'"}), 400
+
+
+    #Clave Redis: user:<user_id>:chat:<chat_id>
+    redis_key = f"user:{user_id}:chat:{chat_id}"
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    key_exists = redis_client.exists(redis_key)
+    
+    if not key_exists:
+        msg = {
+                "role": "system",
+                "content": 
+                    "Eres un asistente de actividades turísticas especializado únicamente en la ciudad de Lima. Solo puedes hablar sobre turismo, lugares, recomendaciones de actividades, cultura y geografía. Si el usuario pregunta algo fuera de este tema, responde diciendo que solo puedes ayudar con temas turísticos."
+                    "Si el usuario pregunta por otra ciudad, debes responder amablemente que solo manejas información sobre Lima. "
+                    "Si el usuario no menciona una ciudad o hace una pregunta ambigua, asume que se refiere a Lima."
+            }
+        redis_client.rpush(redis_key, json.dumps(msg))
+
+    ## Cargar historial de redis
+    context_raw_messages = redis_client.lrange(redis_key, 0, -1)
+    base_messages = [json.loads(m) for m in context_raw_messages]
+    
 
     #######################################
     #######   DETERMINAR INTENCION ########
-    #######################################
 
     prompt = f"""
     Analiza el siguiente mensaje del usuario y responde con una sola palabra que indique la intención:
@@ -36,8 +78,6 @@ def recomendar():
 
     Mensaje: "{query}"
     """
-
-    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
     try:
         response = client.chat.completions.create(
@@ -52,10 +92,10 @@ def recomendar():
     except Exception as e:
         return jsonify({"error": "Error al determinar la intención", "detalle": str(e)}), 500
 
+
+
     if intent == "recomendar":
-        #####################################
         #######   BUSQUEDA VECTORIAL ########
-        #####################################
         recs = db_turismo.similarity_search(query, k=top_k)
         ids = []
         for doc in recs:
@@ -68,14 +108,10 @@ def recomendar():
         recomendados = data[data["id"].isin(ids)][["id", "title_2","description_2"]]
         lugares_lista = recomendados.to_dict(orient="records")
         
-        
-
-        ###################################################################
-        #######   RESPONDER CON MML USANDO CONTEXTO DE LUGARES RECOMENDADOS
-        ###################################################################
-
-        # Genera un prompt para el LLM
+        #######   RESPONDER CON MML USANDO CONTEXTO DE LUGARES RECOMENDADOS  ######
+        #prompt para el LLM
         lista_texto = "\n".join([f"- {lugar['title_2']}: {lugar['description_2']}" for lugar in lugares_lista])
+        
         prompt = f"""
         El usuario está buscando actividades turísticas basados en: "{query}"
 
@@ -85,27 +121,25 @@ def recomendar():
 
         Redáctale una recomendación amable y atractiva mencionando actividades turísticas que se adapten bien a su búsqueda.
         """
-
         
-
         try:
+            ## Agregar mensaje del usuario a Redis
+            base_messages.append({"role": "user", "content": query})
+            redis_client.rpush(redis_key, json.dumps({"role": "user", "content": query}))
+            
             respuesta = client.chat.completions.create(
-            model="gpt-4.1-nano",
-            messages=[
-                {
-                    "role": "system",
-                    "content": (
-                        "Eres un asistente de actividades turísticas especializado únicamente en la ciudad de Lima. Solo puedes hablar sobre turismo, lugares, recomendaciones de actividades, cultura y geografía. Si el usuario pregunta algo fuera de este tema, responde diciendo que solo puedes ayudar con temas turísticos."
-                        "Si el usuario pregunta por otra ciudad, debes responder amablemente que solo manejas información sobre Lima. "
-                        "Si el usuario no menciona una ciudad o hace una pregunta ambigua, asume que se refiere a Lima."
-                    )
-                },
-                {"role": "user", "content": prompt}
-            ],
-            temperature=0.7
+                model="gpt-4.1-nano",
+                messages = base_messages,
+                temperature=0.7
             )
-
+            ## Agregar mensaje de system a Redis
             recomendacion = respuesta.choices[0].message.content.strip()
+            redis_client.rpush(redis_key, json.dumps({"role": "assistant", "content": recomendacion}))
+            
+            return jsonify({
+                "recomendacion": recomendacion,
+                "lugares": lugares_lista
+            })
 
         except Exception as e:
             return jsonify({
@@ -113,37 +147,34 @@ def recomendar():
                 "detalle": str(e)
             }), 500
 
-        
-        #####################################################
-        #######   BUSQUEDA VECTORIAL 
-        #####################################################
-        
-        return jsonify({
-            "recomendacion": recomendacion,
-            "lugares": lugares_lista
-        })
+    else: ## Caso donde no busca recomendación.
 
-    else:
+        base_messages.append({"role": "user", "content": query})
+        redis_client.rpush(redis_key, json.dumps({"role": "user", "content": query}))
+
         respuesta = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": (
-                    "Eres un asistente turístico especializado únicamente en la ciudad de Lima. Solo puedes hablar sobre turismo, lugares, recomendaciones de actividades, cultura y geografía. Si el usuario pregunta algo fuera de este tema, responde diciendo que solo puedes ayudar con temas turísticos."
-                    "Si el usuario pregunta por otra ciudad, debes responder amablemente que solo manejas información sobre Lima. "
-                    "Si el usuario no menciona una ciudad o hace una pregunta ambigua, asume que se refiere a Lima."
-                )
-            },
-            {"role": "user", "content": query}
-        ],
-        temperature=0.7
+            model="gpt-3.5-turbo",
+            messages = base_messages,
+            temperature=0.7
         )
+        
+        ## Agregar mensaje de system a Redis
+        rpta_txt = respuesta.choices[0].message.content.strip()
+        redis_client.rpush(redis_key, json.dumps({"role": "assistant", "content": rpta_txt}))
+            
         return jsonify({
             "intencion": intent,
-            "respuesta": respuesta.choices[0].message.content.strip()
+            "respuesta": rpta_txt
         })
     
+@app.route('/chat/<key>', methods=['DELETE'])
+def borrar_conversacion(key):
+    if redis_client.exists(key):
+        redis_client.delete(key)
+        return jsonify({"message": "Conversación eliminada"}), 200
+    else:
+        return jsonify({"error": "Clave no encontrada"}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
